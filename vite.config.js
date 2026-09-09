e4869606132f767b513ce2a055cb44657cbe1d14
kewasoftwere-sudo <kewasoftwere@gmail.com>
2026-09-09 08:55:29 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -12,4 +12,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
